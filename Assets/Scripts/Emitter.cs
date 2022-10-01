a3a682080d1767b311d1787cb6f08d39ae92eccc
TheDollarGameStore <thedollargamestore@gmail.com>
2022-10-01 13:03:29 +0200

    Emitter logic stage 1

--- Assets/Scripts/Emitter.cs
+++ Assets/Scripts/Emitter.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Emitter : MonoBehaviour
 {
-    private LuminColor upColor;
-    private LuminColor downColor;
-    private LuminColor rightColor;
-    private LuminColor leftColor;
+    [HideInInspector] public LuminColor upColor;
+    [HideInInspector] public LuminColor downColor;
+    [HideInInspector] public LuminColor rightColor;
+    [HideInInspector] public LuminColor leftColor;
+
+    [SerializeField] private GameObject blueLuminPrefab;
+    [SerializeField] private GameObject redLuminPrefab;
+    [SerializeField] private GameObject yellowLuminPrefab;
+    [SerializeField] private GameObject greenLuminPrefab;
 
     [SerializeField] private GameObject emitterDiodePrefab;
 
@@ -26,9 +31,9 @@ public class Emitter : MonoBehaviour
         leftColor = (LuminColor)Random.Range(0, 5);
 
         AddDiode(upColor, 0f);
-        AddDiode(rightColor, 90f);
+        AddDiode(rightColor, 270f);
         AddDiode(downColor, 180f);
-        AddDiode(leftColor, 270f);
+        AddDiode(leftColor, 90f);
     }
 
     void AddDiode(LuminColor color, float angle)
@@ -40,5 +45,62 @@ public class Emitter : MonoBehaviour
         GameObject newDiode = Instantiate(emitterDiodePrefab, transform.position, Quaternion.identity, transform);
         newDiode.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
         newDiode.GetComponent<SpriteRenderer>().color = Colors.GetColorFromEnum(color);
+    }
+
+    public void Placed(int y, int x)
+    {
+        LaunchLumins(y, x);
+        Invoke("DestroySelf", 1f);
+    }
+
+    private void LaunchLumins(int y, int x)
+    {
+        if (upColor != LuminColor.NONE)
+        {
+            PlaceLumin(y + 1, x, upColor);
+        }
+
+        if (downColor != LuminColor.NONE)
+        {
+            PlaceLumin(y - 1, x, downColor);
+        }
+
+        if (leftColor != LuminColor.NONE)
+        {
+            PlaceLumin(y, x - 1, leftColor);
+        }
+
+        if (rightColor != LuminColor.NONE)
+        {
+            PlaceLumin(y, x + 1, rightColor);
+        }
+    }
+
+    private void PlaceLumin(int y, int x, LuminColor color)
+    {
+        Lumin newLumin = null;
+
+        switch (color)
+        {
+            case LuminColor.RED:
+                newLumin = Instantiate(redLuminPrefab, transform.position, Quaternion.identity).GetComponent<Lumin>();
+                break;
+            case LuminColor.YELLOW:
+                newLumin = Instantiate(yellowLuminPrefab, transform.position, Quaternion.identity).GetComponent<Lumin>();
+                break;
+            case LuminColor.GREEN:
+                newLumin = Instantiate(greenLuminPrefab, transform.position, Quaternion.identity).GetComponent<Lumin>();
+                break;
+            case LuminColor.BLUE:
+                newLumin = Instantiate(blueLuminPrefab, transform.position, Quaternion.identity).GetComponent<Lumin>();
+                break;
+        }
+
+        GameManager.instance.tiles[y, x].lumin = newLumin;
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
